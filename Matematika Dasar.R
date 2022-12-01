8a44084a980eb36834c5d0cc62b2bfe200c5083c
Muhammad Athanabil Andi Fawazdhia <80562205+fawazdhianabil@users.noreply.github.com>
2022-12-01 12:13:18 +0700

    Add files via upload

---
+++ Matematika Dasar.R
@@ -0,0 +1,34 @@
+#Nomor 1
+Vol*kubus = 123
+Vol-kubus = 123
+Vol.kubus = 123
+_volkubus = 123
+Vol_kubus = 123
+
+#Nomor 2
+#2.a
+X = ((4^5-2)^(1/6))/4
+X
+
+#2.b
+Y = sqrt(9-(5/2)^2+6)
+Y
+
+#Nomor 3
+#Persamaan Pythagoras
+# a^2 + b^2 = c^2
+a = 3
+b = 4
+c = 5
+a^2 + b^2 == c^2
+
+
+#Persamaan Linear
+#ax + b = 0
+a = -3
+x = 1
+b = 3
+a*x + b == 0
+
+
+
---
+++ Sequences.R
@@ -0,0 +1,15 @@
+#urutan 10 sampai 20 dengan interval kenaikan 1
+a <- seq(10,20,by=1)
+a
+
+#urutan 10 sampai 20 dengan interval kenaikan 0.5
+b <- seq(10,20,by=0.5)
+b
+
+#urutan 20 sampai 1 dengan interval penurunan 1
+c <- seq(20,1,by=-1)
+c
+
+#urutan 20 sampai 1 dengan interval penurunan 0.5
+d <- seq(20,1,by=-0.5)
+d
